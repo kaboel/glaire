4ebb480361c1a28def313c194b42ced3ac7cc152
Faiq <faiq.kaboel@gmail.com>
2019-07-10 04:13:35 +0700

    Add Book model

---
+++ server/src/_models/Book.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const SCHEMA = mongoose.Schema({
+    authorId: {
+        type: String,
+        required: true
+    },
+    publisherId: {
+        type: String,
+        required: true
+    },
+    title: {
+        type: String,
+        required: true
+    },
+    genre: {
+        type: String,
+        required: true
+    },
+    published: {
+        type: Date,
+        required: true
+    }
+});
+
+const Book = mongoose.model('Book', SCHEMA, 'Books');
+
+module.exports = Book;
