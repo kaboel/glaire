906ca6ba0dbc176adee422ef16dc41ee64ce4d7d
Faiq <faiq.kaboel@gmail.com>
2019-07-10 04:12:46 +0700

    Fix database uriString

--- server/src/_config/config.js
+++ server/src/_config/config.js
@@ -2,6 +2,6 @@ module.exports = {
     name: 'glaire-server',
     port: '8089',
     db: {
-        uriString: `mongodb+srv://glaire-user:4gsxJ2UA8lvt77ii@kluster-0-co84f.mongodb.net/test?retryWrites=true&w=majority`
+        uriString: `mongodb+srv://glaire-user:4gsxJ2UA8lvt77ii@kluster-0-co84f.mongodb.net/glaire?retryWrites=true&w=majority`
     }
 };
