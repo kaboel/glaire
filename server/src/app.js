d2340729c00417597d761d3323bf3f707673e33c
Faiq <faiq.kaboel@gmail.com>
2019-07-09 08:45:47 +0700

    Connect server to MongoDB Atlas

--- server/src/app.js
+++ server/src/app.js
@@ -3,14 +3,28 @@ const cors = require('cors');
 const morgan = require('morgan');
 const gqlHTTP = require('express-graphql');
 const SCHEMA = require('./_schema/schema');
+const mongoose = require('mongoose');
+const config = require('./_config/config');
 
 const App = express();
 
 App.use(morgan('combined'));
 App.use(cors());
 
-App.use('/gql', gqlHTTP({ schema: SCHEMA, graphiql: true }));
+App.use('/v1', gqlHTTP({ schema: SCHEMA, graphiql: true }));
 
-App.listen(3000, () => {
-    console.log('Server started on port 3000');
-});
+const db = config.db.uriString;
+mongoose.connect(db, {useNewUrlParser: true})
+    .then(() => {
+        console.log('Database connected on Kluster-0@glaire...');
+        try {
+            App.listen(process.env.PORT || config.port, () => {
+                console.log(`Server started on port: ${config.port}...`);
+            });
+        } catch (err) {
+            console.log(err.message);
+        }
+    }).catch((err) => {
+        console.log(`Error while connecting to database - ${err.message}`);
+    });
+
